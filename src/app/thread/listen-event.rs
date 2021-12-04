93e3bd9bc65212836f44cdc255d34d995f20fa75
Oscar Sancheze <oscarricardosan@gmail.com>
2021-12-04 13:54:01 -0500

    Se separa la capa de los layout para renders.

--- src/app/thread/listen-event.rs
+++ src/app/thread/listen-event.rs
@@ -1,46 +1,48 @@
+use std::cell::RefCell;
 use std::error::Error;
 use std::io::Stdout;
+use std::ops::Deref;
 use std::process::exit;
 use std::rc::Rc;
 use std::sync::mpsc::Receiver;
 use crossterm::event;
 use crossterm::event::{Event as CrosstermEvent, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal::disable_raw_mode;
-use tui::backend::CrosstermBackend;
+use tui::backend::{Backend, CrosstermBackend};
 use tui::Terminal;
-use crate::Event;
+use crate::{Event, Menu, StateApp};
 
 pub struct ThreadListenEvent {
 }
 
 impl ThreadListenEvent {
-    pub fn handle(
+    pub fn handle<T: Backend>(
         rx: Rc<Receiver<Event>>,
-        terminal: &mut Terminal<CrosstermBackend<Stdout>>
+        state_app: Rc<RefCell<StateApp<T>>>
     ) -> Result<(), Box<dyn Error>>  {
 
         match rx.recv()? {
             Event::Input(event) => match event {
                 event::KeyEvent {
-                    code: KeyCode::Char('s'),
+                    code: KeyCode::Char('a'),
                     modifiers: KeyModifiers::CONTROL,
                 }=> {
-                    panic!("mori");
+                    state_app.borrow_mut().current_menu= Menu::Help;
                 }
                 event::KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: KeyModifiers::NONE,
+                    code: KeyCode::Char('i'),
+                    modifiers: KeyModifiers::CONTROL,
+                }=> {
+                    state_app.borrow_mut().current_menu= Menu::Main;
+                }
+                event::KeyEvent {
+                    code: KeyCode::Char('s'),
+                    modifiers: KeyModifiers::CONTROL,
                 } => {
                     disable_raw_mode()?;
-                    terminal.show_cursor()?;
-                    terminal.clear();
+                    state_app.borrow_mut().terminal.show_cursor()?;
+                    state_app.borrow_mut().terminal.clear();
                     exit(0);
-                }
-                event::KeyEvent {
-                    code: KeyCode::Char('a'),
-                    modifiers: KeyModifiers::NONE,
-                } => {
-                    panic!("mori");
                 }
                 _ => {}
             },
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::sync::mpsc;
 use std::{io, thread};
 use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::error::Error;
 use std::rc::Rc;
 use std::time::{Duration, Instant};
 use crossterm::event;
 use crossterm::event::{Event as CrosstermEvent, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use tui::backend::CrosstermBackend;
+use tui::backend::{Backend, CrosstermBackend};
 use tui::layout::{Alignment, Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
 use tui::Terminal;
@@ -18,6 +19,8 @@ use crate::sender_event::ThreadSendEvent;
 
 #[path = "app/resources/layout/main.rs"]
 mod layout_main;
+#[path = "app/resources/layout/help.rs"]
+mod layout_help;
 
 #[path = "app/conifg/config_render.rs"]
 mod config_render;
@@ -27,10 +30,19 @@ mod sender_event;
 #[path = "app/thread/listen-event.rs"]
 mod listen_event;
 
-
 pub enum Event{
     Input(KeyEvent),
     Tick,
+}
+
+pub struct StateApp<T: Backend> {
+    current_menu: Menu,
+    terminal: Terminal<T>,
+}
+#[derive(Copy, Clone)]
+pub enum Menu{
+    Main,
+    Help,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -39,21 +51,39 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rx= Rc::new(rx);
     ThreadSendEvent::handle(tx);
 
-    enable_raw_mode().expect("cant run in raw mode");
-
-
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
+
+    let mut state_app= Rc::new(
+        RefCell::new(
+            StateApp{
+                current_menu: Menu::Main,
+                terminal,
+            }
+        )
+    );
+
+    enable_raw_mode().expect("cant run in raw mode");
+    state_app.borrow_mut().terminal.clear();
+
 
     loop {
-        terminal.draw(|frame| {
-            let layout_main= layout_main::LayoutMain::new();
-            layout_main.render(frame);
+        let current_menu= state_app.borrow_mut().current_menu;
+        state_app.borrow_mut().terminal.draw(|frame| {
+            match current_menu {
+                Menu::Main=> {
+                    let layout= layout_main::LayoutMain::new();
+                    layout.render(frame);
+                }
+                Menu::Help=> {
+                    let layout= layout_help::LayoutHelp::new();
+                    layout.render(frame);
+                }
+            }
         })?;
 
-        ThreadListenEvent::handle(rx.clone(), &mut terminal);
+        ThreadListenEvent::handle(rx.clone(), state_app.clone());
     }
 
     Ok(())
